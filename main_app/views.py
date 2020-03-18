bde9fae7096c2d6b08b831453d2db0bca01cc2fb
ManliestBen <benjamintmanley@gmail.com>
2020-03-18 10:41:41 -0500

    add model and migrate

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Dog
+
 
 def home(request):
     return HttpResponse('<h1>Hello --BARK-- </h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
